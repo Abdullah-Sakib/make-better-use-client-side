3ef4b672164a84c8467846111f25aeba43c48392
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-11-23 23:52:40 +0600

    Categories section

---
+++ src/Pages/Home/Categories/Categories.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const Categories = () => {
+  return (
+    <div className="px-24 mb-32 mt-12">
+      <h1 className="text-2xl font-semibold text-center mb-8">Categories section</h1>
+      <div className="grid grid-cols-3 gap-10">
+
+
+        <div className="card card-compact  bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="card card-compact bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="card card-compact  bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Advertisement from '../Advertisement/Advertisement';
 import Banner from '../Banner/Banner';
+import Categories from '../Categories/Categories';
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Advertisement></Advertisement>
+      <Categories></Categories>
     </div>
   );
 };
