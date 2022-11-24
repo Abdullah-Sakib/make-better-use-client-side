463c74c9327d2c6717d0bd104b6424d42dbf5cf3
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-11-24 07:45:48 +0600

    Added react-hook-form in the register page

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const handleCreateUser = data => {
+    console.log(data);
+  }
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col ">
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="card-body w-96">
+            <form onSubmit={handleSubmit(handleCreateUser)} className="card-body w-96">
               <h1 className="text-2xl font-bold text-center">Register</h1>
               <div className="form-control">
                 <label className="label">
@@ -17,33 +22,40 @@ const Register = () => {
                   type="text"
                   placeholder="full name"
                   className="input input-bordered"
+                  {...register("fullname", { required: "Please provide your full name" })} 
                 />
+                 {errors.fullname && <p className="text-error" role="alert">{errors.fullname?.message}</p>}
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
-                  type="text"
+                  type="email"
                   placeholder="email"
                   className="input input-bordered"
+                  {...register("email", { required: "Please provide your email" })} 
                 />
+                 {errors.email && <p className="text-error" role="alert">{errors.email?.message}</p>}
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
                 <input
-                  type="text"
+                  type="password"
                   placeholder="password"
                   className="input input-bordered"
+                  {...register("password", { required: "Please provide your password" })} 
                 />
+                 {errors.password && <p className="text-error" role="alert">{errors.password?.message}</p>}
               </div>
               <div className="form-control">
                 <label className="label cursor-pointer justify-start">
                   <input
                     type="checkbox"
                     className="checkbox checkbox-primary"
+                    {...register("seller")} 
                   />
                   <span className="label-text ml-4">Seller Account</span>
                 </label>
@@ -57,7 +69,7 @@ const Register = () => {
                   Login
                 </Link>
               </p>
-            </div>
+            </form>
           </div>
         </div>
       </div>
