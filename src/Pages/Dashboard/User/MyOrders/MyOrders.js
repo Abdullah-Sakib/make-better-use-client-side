1d86d3336a73a64120c8dc4a46201508bbf483d8
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-11-25 19:10:30 +0600

    Get orders using react query

--- src/Pages/Dashboard/User/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/User/MyOrders/MyOrders.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React, { useEffect } from "react";
 
 const MyOrders = () => {
+  const { data:orders = [], isLoading } = useQuery({
+    queryKey: ["myorders"],
+    queryFn: () =>
+      fetch(`http://localhost:5000/bookedProducts`, {
+        method: "GET",
+        headers: {
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+      }).then((res) => res.json()),
+  });
+
   return (
     <div>
       <h2>My orders</h2>
+      {orders?.map((order) => (
+        <li key={order._id}>{order.userName}</li>
+      ))}
     </div>
   );
 };
